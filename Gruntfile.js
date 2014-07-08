cd943d27fef98b4544e45443fcdd6c32ccf069d7
Weslley Araujo <wparaujo@sp.r7.com>
2014-07-08 13:20:45 -0300

    retab

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,90 +1,90 @@
 module.exports = function (grunt) {
-	'use strict';
+'use strict';
 
-	var tasks = [
-		'grunt-contrib-jshint',
-		'grunt-contrib-compass',
-		'grunt-contrib-watch',
-		'grunt-contrib-jasmine',
-		'grunt-concat-css'
-	];
+  var tasks = [
+    'grunt-contrib-jshint',
+    'grunt-contrib-compass',
+    'grunt-contrib-watch',
+    'grunt-contrib-jasmine',
+    'grunt-concat-css'
+  ];
 
-	grunt.initConfig({
-		watch: {
-			js: {
-				files: [
-					'lib/*.js',
-					'lib/**/*.js',
-					'spec/*.js',
-				],
-				tasks: [
-					'jshint',
-					'jasmine'
-				]
-			},
-			css: {
-				files: [
-					'assets/sass/*.sass',
-					'assets/sass/**/*.sass',
-					'assets/css/**/*.css'
-				],
-				tasks: [
-					'compass:dev',
-					'concat_css'
-				]
-			}
-		},
-		concat_css: {
-			all: {
-				src: [
-					'assets/css/modules/*.css',
-					'assets/css/fonts/**/*.css'
-				],
-				dest: 'assets/css/app.css'
-			}
-		},
-		jshint: {
-			all: [
-				'Gruntfile.js',
-				'assets/javascripts/*.js',
-				'specs/*.js'
-			]
-		},
-		compass: {
-			dev: {
-				options: {
-					sassDir: 'assets/sass',
-					cssDir: 'assets/css',
-					imagesDir: 'assets/images',
-					environment: 'development'
-				}
-			},
-			prod: {
-				options: {
-					sassDir: 'assets/sass',
-					cssDir: 'assets/css',
-					imagesDir: 'assets/images',
-					outputStyle: 'compressed',
-					noLineComments: true,
-					environment: 'production'
-				}
-			}
-		},
-		jasmine: {
-			src: [
-				'lib/*.js',
-				'lib/**/*.js',
-			],
-			options: {
-				specs: 'spec/*.js',
-				vendor: [
-					"./bower_components/jquery/dist/jquery.min.js",
-					"./bower_components/underscore/underscore.js"
-				]
-			}
-		},
-	});
+grunt.initConfig({
+  watch: {
+    js: {
+      files: [
+        'lib/*.js',
+        'lib/**/*.js',
+        'spec/*.js',
+      ],
+      tasks: [
+        'jshint',
+        'jasmine'
+      ]
+    },
+    css: {
+      files: [
+        'assets/sass/*.sass',
+        'assets/sass/**/*.sass',
+        'assets/css/**/*.css'
+      ],
+      tasks: [
+        'compass:dev',
+        'concat_css'
+      ]
+    }
+  },
+  concat_css: {
+    all: {
+      src: [
+        'assets/css/modules/*.css',
+        'assets/css/fonts/**/*.css'
+      ],
+      dest: 'assets/css/app.css'
+    }
+  },
+  jshint: {
+    all: [
+      'Gruntfile.js',
+      'assets/javascripts/*.js',
+      'specs/*.js'
+    ]
+  },
+  compass: {
+    dev: {
+      options: {
+        sassDir: 'assets/sass',
+        cssDir: 'assets/css',
+        imagesDir: 'assets/images',
+        environment: 'development'
+      }
+    },
+    prod: {
+      options: {
+        sassDir: 'assets/sass',
+        cssDir: 'assets/css',
+        imagesDir: 'assets/images',
+        outputStyle: 'compressed',
+        noLineComments: true,
+        environment: 'production'
+      }
+    }
+  },
+  jasmine: {
+    src: [
+      'lib/*.js',
+      'lib/**/*.js',
+    ],
+    options: {
+      specs: 'spec/*.js',
+      vendor: [
+        "./bower_components/jquery/dist/jquery.min.js",
+        "./bower_components/underscore/underscore.js"
+      ]
+    }
+  },
+});
 
-	tasks.forEach(grunt.loadNpmTasks);
-	grunt.registerTask('default', ['jshint']);
+tasks.forEach(grunt.loadNpmTasks);
+grunt.registerTask('default', ['jshint']);
 };
